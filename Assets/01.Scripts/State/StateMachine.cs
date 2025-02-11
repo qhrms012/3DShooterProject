ee7d3e848c7e6f7a4223e5a2741336fcb418b95e
boguen <qhrms012@naver.com>
2025-02-11 16:21:38 +0900

    [add] 인풋시스템,상태머신

---
+++ Assets/01.Scripts/State/StateMachine.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class StateMachine
+{
+    private Istate currentState;
+
+    public void SetState(Istate newState)
+    {
+        if (currentState != null)
+            currentState.Exit();
+
+        currentState = newState;
+        currentState.Enter();
+    }
+
+    public void Update(Vector3 playerVector)
+    {
+        if (currentState != null)
+            currentState.Execute(playerVector);
+    }
+}
