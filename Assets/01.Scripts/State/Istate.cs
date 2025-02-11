a0267bf7ec6625834bbe434cda67fc6ae03bf70d
boguen <qhrms012@naver.com>
2025-02-11 16:18:08 +0900

    [add] 제네릭싱글톤, Istate

---
+++ Assets/01.Scripts/State/Istate.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface Istate
+{
+    void Enter();
+    void Execute(Vector3 playerVector);
+    void Exit();
+}
