79c396679890d76353d420cf00714b5560efdd3b
boguen <qhrms012@naver.com>
2025-03-04 20:25:19 +0900

    [add] ui및 버튼 연결 및 아이템 사기 구현

--- Assets/01.Scripts/Shop.cs
+++ Assets/01.Scripts/Shop.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Shop : MonoBehaviour
@@ -7,13 +8,21 @@ public class Shop : MonoBehaviour
     public RectTransform uiGroup;
     public Animator childAnimator;
 
+
+    public GameObject[] itemObj;
+    public int[] itemPrice;
+    public Transform[] itemPos;
+
+    public string[] talkData;
+    public TextMeshProUGUI talkText;
+
     Player enterPlayer;
 
     public void Enter(Player player)
     {
         enterPlayer = player;
         uiGroup.anchoredPosition = Vector3.zero;
-        Debug.Log("µé¾î¿È");
+        
     }
 
 
@@ -21,6 +30,30 @@ public class Shop : MonoBehaviour
     {
         childAnimator.Play("Hello");
         uiGroup.anchoredPosition = Vector3.down * 1000;
+
+    }
+    public void Buy(int index)
+    {
+        int price = itemPrice[index];
+        if(price > enterPlayer.coin)
+        {
+            StopCoroutine(Talk());
+            StartCoroutine(Talk());
+            return;
+        }
+
+        enterPlayer.coin -= price;
+        Vector3 ranVec = Vector3.right * Random.Range(-3, 3)
+                                    + Vector3.forward * Random.Range(-3, 3);
+        Instantiate(itemObj[index], itemPos[index].position + ranVec, itemPos[index].rotation);
+    }
+    IEnumerator Talk()
+    {
+        talkText.text = talkData[1];
+        yield return new WaitForSeconds(2f);
+        talkText.text = talkData[0];
+
+
 
     }
 }
