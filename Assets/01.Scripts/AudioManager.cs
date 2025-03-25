dd745e9fca266fe35ca52cacd68d4e556493dfbf
boguen <qhrms012@naver.com>
2025-03-25 19:51:38 +0900

    [add] 마스터볼륨 sfx bgm 설정 완료

--- Assets/01.Scripts/AudioManager.cs
+++ Assets/01.Scripts/AudioManager.cs
@@ -1,18 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
-public class AudioManager : MonoBehaviour
+public class AudioManager : Singleton<AudioManager>
 {
+    [SerializeField] private AudioMixer audioMixer;
+    [SerializeField] private Slider masterSlider;
+    [SerializeField] private Slider bgmSlider;
+    [SerializeField] private Slider sfxSlider;
+
+
+    private void Awake()
+    {
+        masterSlider.onValueChanged.AddListener(SetMasterVolume);
+        bgmSlider.onValueChanged.AddListener(SetBgmVolume);
+        sfxSlider.onValueChanged.AddListener(SetSfxVolume);
+
+    }
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetMasterVolume(float volume)
     {
-        
+        audioMixer.SetFloat("Master", Mathf.Log10(volume) * 20);
     }
+    public void SetBgmVolume(float volume)
+    {
+        audioMixer.SetFloat("BGM", Mathf.Log10(volume) * 20);
+    }
+    public void SetSfxVolume(float volume)
+    {
+        audioMixer.SetFloat("SFX", Mathf.Log10(volume) * 20);
+    }
+}
+public enum SFX
+{
+    Shot,
+    Reload,
+    Hit,
+    GameOver
 }
